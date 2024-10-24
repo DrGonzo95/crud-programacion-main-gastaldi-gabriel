b0aa676c970bda8be8d9f33b160f1e7ed1307183
DrGonzo95 <gabriel.gastaldi.95@gmail.com>
2024-10-23 22:05:16 -0300

    first commit

---
+++ src/auth-config.js
@@ -0,0 +1,6 @@
+// src/auth-config.js
+export const authConfig = {
+    domain: 'dev-hdaogwsqsutayp8p.us.auth0.com',
+    clientId: '2p5EKv3NmwAU0tFLt1QvTgO0Az2Y172V',
+    redirectUri: window.location.origin, // Cambia esto si necesitas un URI diferente
+};
